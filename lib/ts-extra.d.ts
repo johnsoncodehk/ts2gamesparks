ea01db7673ae09b7b88bfb1ad247c4610aaa0a1c
johnsoncodehk <johnsoncodehk@gmail.com>
2020-08-22 18:33:41 +0800

    Remove build files

--- lib/ts-extra.d.ts
+++
@@ -1,3 +0,0 @@
-import * as ts from "typescript";
-export declare function getTsConfig(dir: string): ts.ParsedCommandLine;
-export declare function getLanguageService(tsConfig: ts.ParsedCommandLine, dir: string): ts.LanguageService;
